4f50d0a139638647e02a626506ed1f802b6ec489
Niraj Kumar Pandey <nirajpandey034@gmail.com>
2024-03-17 18:38:16 +0530

    Default route setup

---
+++ users/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.get('*', (req, res) => {
+  return res.json({ info: 'Hi! You have reached Users MS.' });
+});
+
+app.listen(process.env.APP_PORT, (res, err) => {
+  if (err) {
+    console.log('Something went wrong');
+  }
+  console.log(`Server Started successfully at ${process.env.APP_PORT}`);
+});
